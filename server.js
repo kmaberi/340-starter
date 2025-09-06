0d8b6a270d8aa3fddbeadf5d6d3e042a6ace4752
kmaberi <kennethmaberi@gmail.com>
2025-09-06 13:10:26 +0300

    MY first commit

--- server.js
+++ server.js
@@ -2,25 +2,49 @@
  * This server.js file is the primary file of the 
  * application. It is used to control the project.
  *******************************************/
+
+
+/* ***********************
+ * View Engine and Templates
+ *************************/
+
+
+app.set("view engine", "ejs")
+app.use(expressLayouts)
+app.set("layout", "./layouts/layout")
+
+
 /* ***********************
  * Require Statements
  *************************/
 const express = require("express")
-const env = require("dotenv").config()
+const expressLayouts = require("express-ejs-layouts")
+require("dotenv").config()
 const app = express()
 const static = require("./routes/static")
+
+// index route
+app.get("/", (req, res) => {res.render("index", {title: "Home"})})
+
 
 /* ***********************
  * Routes
  *************************/
 app.use(static)
 
+/* ******************************************
+ * Default GET route
+ * ***************************************** */
+app.get("/", (req, res) => {
+  res.send("Welcome home!")
+})
+
 /* ***********************
  * Local Server Information
  * Values from .env (environment) file
  *************************/
-const port = process.env.PORT
-const host = process.env.HOST
+const port = process.env.PORT || 3000
+const host = process.env.HOST || "localhost"
 
 /* ***********************
  * Log statement to confirm server operation
