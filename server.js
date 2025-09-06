63f99fa357bcffb88d33df2954ebe869443f4762
kmaberi <kennethmaberi@gmail.com>
2025-09-06 13:46:59 +0300

    sie

--- server.js
+++ server.js
@@ -1,20 +1,28 @@
-const express = require("express")
-const path = require("path")
+/* ******************************************
+ * This is the application server
+ * ******************************************/
+/* Bring Express into scope and create the app */
+const express = require("express");
 
-const app = express()
+const app = express();
 
-// Now configure after defining app
-app.set("view engine", "ejs")
-app.set("views", path.join(__dirname, "views"))
 
-// Middleware, routes, etc. go here
-app.use(express.static("public"))
 
-app.get("/", (req, res) => {
-  res.send("Hello from Render!")
-})
 
-const PORT = process.env.PORT || 3000
+/* ******************************************
+ * Default GET route
+ * ***************************************** */
+app.get("/", (req, res) => { res.send("Welcome home!"); });
+
+/* ******************************************
+ * Server host name and port
+ * ****************************************** */
+const HOST = "localhost";
+const PORT = process.env.PORT || 3000;
+
+/* ***********************
+ * Log statement to confirm server operation
+ * *********************** */
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
-})
+  console.log(`trial app listening on ${HOST}:${PORT}`);
+});
