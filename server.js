44cce3b5f5f049765ea4d14ffed1591f4f24d17d
kmaberi <kennethmaberi@gmail.com>
2025-09-10 22:04:11 +0300

    review

--- server.js
+++ server.js
@@ -3,11 +3,22 @@
  * ******************************************/
 /* Bring Express into scope and create the app */
 const express = require("express");
+const expressLayouts = require("express-ejs-layouts");
+require("dotenv").config();
 
 const app = express();
 
+// Set the views directory
+app.set("views", "./views");
 
+// Set the view engine to EJS
+app.set("view engine", "ejs");
 
+// Use express-ejs-layouts
+app.use(expressLayouts);
+
+// Static files middleware
+app.use(express.static("public"));
 
 /* ******************************************
  * Default GET route
@@ -26,5 +37,5 @@ const PORT = process.env.PORT || 3000;
  * Log statement to confirm server operation
  * *********************** */
 app.listen(PORT, () => {
-  console.log(`trial app listening on ${HOST}:${PORT}`);
+  console.log(`Server running on http://${HOST}:${PORT}`);
 });
