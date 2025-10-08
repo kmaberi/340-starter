c23f7d08b49b65186cc17386f391ae3e7bcdbc6b
kenneth maberi <kennethmaberi@gmail.com>
2025-10-08 15:39:30 +0300

    vv

--- database/db-sql-code.sql
+++ database/db-sql-code.sql
@@ -1,4 +1,5 @@
-CREATE TYPE public.account_type AS ENUM
+
+DB_SSL=trueCREATE TYPE public.account_type AS ENUM
     ('Client', 'Employee', 'Admin');
 
 ALTER TYPE public.account_type
--- database/pool.js
+++ database/pool.js
@@ -1,13 +1,15 @@
-console.log('pool.js loaded')
 
+console.log('pool.js loaded');
 require('dotenv').config();
 const { Pool } = require('pg');
 
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  ssl: process.env.PGSSLMODE === 'require'
-    ? { rejectUnauthorized: false }
-    : false,
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  database: process.env.DB_NAME,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  ssl: process.env.DB_SSL === 'true'
 });
 
 // Smoke-test query
